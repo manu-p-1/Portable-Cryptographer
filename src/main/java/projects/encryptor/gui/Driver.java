0e14c5fad3883779128e03895ce6d84bcb923bff
Manu <28782438+manu-p-1@users.noreply.github.com>
2019-01-05 22:18:50 -0500

    Restructured project. Use of FXML and SceneBuilder yields legible code

---
+++ src/main/java/projects/encryptor/gui/Driver.java
@@ -0,0 +1,25 @@
+package projects.encryptor.gui;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Driver extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+	
+		Parent root = FXMLLoader.load(getClass().getResource("CryptographerGUI.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Portable Cryptographer");
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
---
+++ src/main/java/projects/encryptor/gui/FXMLController.java
@@ -0,0 +1,99 @@
+package projects.encryptor.gui;
+
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.NoSuchPaddingException;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+import projects.encryptor.*;
+
+public class FXMLController {
+	
+	@FXML private CheckMenuItem modeEncrypt, modeDecrypt;
+	@FXML private TextArea taPlainText, taCrypticResult;
+	@FXML private Button copyBtn, pasteBtn, resetBtn, cryptosystemVariableBtn;
+	private Cryptographer cryptographer;
+	private final Clipboard clipboard = Clipboard.getSystemClipboard();
+	private final ClipboardContent content = new ClipboardContent();
+	
+	public FXMLController() {
+		try {
+			cryptographer = new Cryptographer();
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+			System.err.println(e + " occured.");
+			System.exit(1);
+		}
+	}
+	
+	public void closeMenuItemOnAction() {
+		Platform.exit();
+		System.exit(0);
+	}
+	
+	public void checkMenuItemEncryptOnAction() {
+		modeDecrypt.setSelected(false);
+		clearTextAreas();
+		taPlainText.setPromptText("Enter Text");
+		cryptosystemVariableBtn.setText("Encrypt");
+	}
+	
+	public void checkMenuItemDecryptOnAction() {
+		modeEncrypt.setSelected(false);
+		clearTextAreas();
+		taPlainText.setPromptText("Enter Encrypted Text");
+		cryptosystemVariableBtn.setText("Decrypt");
+	}
+	
+	public void copyOnMouseClicked() {
+		content.putString(taCrypticResult.getText());
+	    clipboard.setContent(content);
+		copyBtn.setText("Copied!");
+	}
+	
+	public void pasteOnMouseClicked() {
+		taPlainText.setText(clipboard.getString());
+		pasteBtn.setText("Pasted!");
+	}
+	
+	public void resetOnMouseClicked() {
+		copyBtn.setText("Copy Result");
+		pasteBtn.setText("Paste from Clipboard");
+	}
+	
+	private void clearTextAreas() {
+		taPlainText.clear();
+		taCrypticResult.clear();
+	}
+	
+	public void cryptosystemVariableOnMouseClicked() {
+		try {
+			if(modeEncrypt.isSelected()) {
+				String encryptedResult = cryptographer.encrypt(taPlainText.getText());
+				taCrypticResult.setText(encryptedResult);
+			} else {
+				String decryptedResult = cryptographer.decrypt(taPlainText.getText());
+				taCrypticResult.setText(decryptedResult);
+			}
+			resetOnMouseClicked();
+		} catch (Exception e) {
+			alert(taPlainText.getText());
+		}
+	}
+	
+	private void alert(String expression) {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText("Invalid Entry");
+		alert.setContentText("\"" + expression + "\""
+				+ " is not a valid entry.");
+		alert.showAndWait();
+	}
+}
