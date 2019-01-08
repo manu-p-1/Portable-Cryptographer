08ad55b2eeb60688a27cd311de62493ee2aeaacf
Manu <28782438+manu-p-1@users.noreply.github.com>
2019-01-07 23:41:15 -0500

    Updated formatting

--- src/main/java/projects/encryptor/Cryptographer.java
+++ src/main/java/projects/encryptor/Cryptographer.java
@@ -18,23 +18,23 @@ import java.security.NoSuchAlgorithmException;
 public class Cryptographer implements BasicCryptosystem {
 
 	private boolean isEncrpyted;
-  private final String key = "2s5v8x/A?D(G+KbPeShVmYq3t6w9z$B&"; //Randomly generated keys to come later
-  private Key aesKey;
-  private Cipher cipher;
+ 	private final String key = "2s5v8x/A?D(G+KbPeShVmYq3t6w9z$B&"; //Randomly generated keys to come later
+  	private Key aesKey;
+  	private Cipher cipher;
 
 	public Cryptographer() throws NoSuchAlgorithmException, NoSuchPaddingException {
 		isEncrpyted = false;
-    aesKey = new SecretKeySpec(key.getBytes(), "AES");
+    		aesKey = new SecretKeySpec(key.getBytes(), "AES");
 		cipher = Cipher.getInstance("AES");
 	}
 
 	public String encrypt(String plainTxt) throws InvalidKeyException, IllegalBlockSizeException,
 		BadPaddingException, UnsupportedEncodingException {
 		isEncrpyted = true;
-    cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-    byte[] encrypted = cipher.doFinal(plainTxt.getBytes("UTF-8"));
-    String encodedVal = new BASE64Encoder().encode(encrypted);
-    return new String(encodedVal);
+	    	cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+	    	byte[] encrypted = cipher.doFinal(plainTxt.getBytes("UTF-8"));
+		String encodedVal = new BASE64Encoder().encode(encrypted);
+		return new String(encodedVal);
 	}
 
 	public String decrypt(String encrypted) throws InvalidKeyException, IllegalBlockSizeException,
