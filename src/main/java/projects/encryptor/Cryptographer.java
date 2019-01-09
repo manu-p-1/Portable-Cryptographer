c4b528a513065a64a010a4d7b673a80d052e4ac2
Manu <28782438+manu-p-1@users.noreply.github.com>
2019-01-09 13:06:44 -0500

    Some Cryptographer variables have been made final

--- src/main/java/projects/encryptor/Cryptographer.java
+++ src/main/java/projects/encryptor/Cryptographer.java
@@ -19,20 +19,20 @@ public class Cryptographer implements BasicCryptosystem {
 
 	private boolean isEncrpyted;
  	private final String key = "2s5v8x/A?D(G+KbPeShVmYq3t6w9z$B&"; //Randomly generated keys to come later
-  	private Key aesKey;
-  	private Cipher cipher;
+ 	private final Key aesKey;
+ 	private final Cipher cipher;
 
 	public Cryptographer() throws NoSuchAlgorithmException, NoSuchPaddingException {
 		isEncrpyted = false;
-    		aesKey = new SecretKeySpec(key.getBytes(), "AES");
+		aesKey = new SecretKeySpec(key.getBytes(), "AES");
 		cipher = Cipher.getInstance("AES");
 	}
 
 	public String encrypt(String plainTxt) throws InvalidKeyException, IllegalBlockSizeException,
 		BadPaddingException, UnsupportedEncodingException {
 		isEncrpyted = true;
-	    	cipher.init(Cipher.ENCRYPT_MODE, aesKey);
-	    	byte[] encrypted = cipher.doFinal(plainTxt.getBytes("UTF-8"));
+		cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+		byte[] encrypted = cipher.doFinal(plainTxt.getBytes("UTF-8"));
 		String encodedVal = new BASE64Encoder().encode(encrypted);
 		return new String(encodedVal);
 	}
